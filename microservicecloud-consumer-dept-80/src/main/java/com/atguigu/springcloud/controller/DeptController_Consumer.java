8e327365c8b177569eaf787d09b76187ac3541f0
zhanglinchun <2601165134@qq.com>
2019-04-07 16:46:16 +0800

    微服务完善

--- microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/controller/DeptController_Consumer.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/controller/DeptController_Consumer.java
@@ -23,21 +23,23 @@ public class DeptController_Consumer {
     private RestTemplate restTemplate;
 
     @RequestMapping(value = "/consumer/dept/add")
-    public boolean add(Dept dept)
-    {
+    public boolean add(Dept dept) {
         return restTemplate.postForObject(REST_URL_PREFIX + "/dept/add", dept, Boolean.class);
     }
 
     @RequestMapping(value = "/consumer/dept/get/{id}")
-    public Dept get(@PathVariable("id") Long id)
-    {
+    public Dept get(@PathVariable("id") Long id) {
         return restTemplate.getForObject(REST_URL_PREFIX + "/dept/get/" + id, Dept.class);
     }
 
     @SuppressWarnings("unchecked")
     @RequestMapping(value = "/consumer/dept/list")
-    public List<Dept> list()
-    {
+    public List<Dept> list() {
         return restTemplate.getForObject(REST_URL_PREFIX + "/dept/list", List.class);
+    }
+
+    @RequestMapping(value = "/consumer/dept/discovery")
+    public Object discovery() {
+        return restTemplate.getForObject(REST_URL_PREFIX + "/dept/discovery", Object.class);
     }
 }
--- microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/controller/DeptController.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/controller/DeptController.java
@@ -3,6 +3,8 @@ package com.atguigu.springcloud.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +18,9 @@ import com.atguigu.springcloud.service.DeptService;
 public class DeptController {
     @Autowired
     private DeptService service;
+    @Autowired
+    private DiscoveryClient client;
+
 
     @RequestMapping(value = "/dept/add", method = RequestMethod.POST)
     public boolean add(@RequestBody Dept dept) {
@@ -32,5 +37,16 @@ public class DeptController {
         return service.list();
     }
 
+    @RequestMapping(value = "/dept/discovery", method = RequestMethod.GET)
+    public Object discovery() {
+        List<String> list = client.getServices();
+        System.out.println("**********" + list);
 
+        List<ServiceInstance> srvList = client.getInstances("MICROSERVICECLOUD-DEPT");
+        for (ServiceInstance element : srvList) {
+            System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t"
+                    + element.getUri());
+        }
+        return this.client;
+    }
 }
