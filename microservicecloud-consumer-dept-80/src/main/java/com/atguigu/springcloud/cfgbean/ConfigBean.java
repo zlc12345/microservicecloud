e8f67f8ae7b46052ee583f400eda8dbd03f4faf4
zhanglinchun <2601165134@qq.com>
2019-09-02 22:50:36 +0800

    eureka集群

--- microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/cfgbean/ConfigBean.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/cfgbean/ConfigBean.java
@@ -1,5 +1,6 @@
 package com.atguigu.springcloud.cfgbean;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,6 +9,7 @@ import org.springframework.web.client.RestTemplate;
 public class ConfigBean {
 
     @Bean
+    @LoadBalanced//Spring Cloud Ribbon是基于Netflix Ribbon实现的一套客户端       负载均衡的工具
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
     }
