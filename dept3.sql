ca66760b11edb5371f6fe421c8a844336d5aeba9
zhanglinchun <2601165134@qq.com>
2019-09-04 23:06:15 +0800

    sql脚本上传

---
+++ dept3.sql
@@ -0,0 +1,36 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost_3306
+Source Server Version : 50723
+Source Host           : localhost:3306
+Source Database       : clouddb03
+
+Target Server Type    : MYSQL
+Target Server Version : 50723
+File Encoding         : 65001
+
+Date: 2019-09-04 23:03:31
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `dept`
+-- ----------------------------
+DROP TABLE IF EXISTS `dept`;
+CREATE TABLE `dept` (
+  `deptno` bigint(20) NOT NULL AUTO_INCREMENT,
+  `dname` varchar(60) NOT NULL,
+  `db_source` varchar(60) NOT NULL,
+  PRIMARY KEY (`deptno`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of dept
+-- ----------------------------
+INSERT INTO `dept` VALUES ('1', '开发部', 'clouddb03');
+INSERT INTO `dept` VALUES ('2', '人事部', 'clouddb03');
+INSERT INTO `dept` VALUES ('3', '财务部', 'clouddb03');
+INSERT INTO `dept` VALUES ('4', '市场部', 'clouddb03');
+INSERT INTO `dept` VALUES ('5', '运维部', 'clouddb03');
